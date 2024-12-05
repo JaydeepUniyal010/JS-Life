d05ee8b3b80af19a809a31a670e493fe8d007f69
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2024-12-05 13:09:52 +0000

    Font Size

--- 01_StartBasic/03_conversionOperation.js
+++ 01_StartBasic/03_conversionOperation.js
@@ -23,8 +23,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber =55
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
 
 
+// *********************** Operations ***********************
+
+ let value = 3
+ let negValue = -value
+ // console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2/2);
+//  console.log(2%3);
+//  console.log(2**2);
+
+ let str1 = "Hello "
+ let str2 = "Happy"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//  console.log("2" + 3);
+//  console.log(2 + "3");
+//  console.log("2" + 3 +2);
+//  console.log(2 + 3+ "2");
 
 
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+ "");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2+3
+// console.log(num1);
+
+let gameCounter = 100;
+//prefix or postfix
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ 01_StartBasic/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log ("2" >1);
+// console.log("02" >1);
+
+// // always avoid this type code and rembember clean code
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// //always avoid this type code
+// console.log(undefined ==0);
+// console.log(undefined >0);
+// console.log(undefined <=0);
+
+// === Strict Check
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
