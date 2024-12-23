c668a9f12a2fdeaea22cb181b7943042492c1c67
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2024-12-23 17:30:37 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,85 @@
+// const tinderUser = new Object() //singleton
+const tinderUser = {}  //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "yuviii"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "jaydeep",
+            lastname: "uniyal"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+//target {} source-obj
+
+const obj3 = {...obj1, ...obj2} //spread operator-> glass broke
+// console.log(obj3);
+
+//database values
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); //important
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "javascript",
+    price: "999",
+    courseInstructor: "jaydeep"
+}
+
+// course.courseInstructor
+//clean code
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+//API- JSON  syntax intro 
+// {
+//     "name": "jaydeepJSON",
+//     "coursename": "js for everyone",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
