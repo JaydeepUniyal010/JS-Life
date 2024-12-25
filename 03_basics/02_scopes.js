51f59ebdd1dda6a2705eb3efcfbed02540f22409
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2024-12-25 17:45:45 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//NOTE--> NODE scope (code enviroment) OR BROWSER SCOPE ARE DIFFRENT.
+
+
+function one(){
+    const username = "happy"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "happy"
+    if (username === "happy") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
