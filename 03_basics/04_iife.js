cb650928a466fa1cf670dc25a0ab69f3c17216d6
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2024-12-26 16:33:26 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})(); //semi colon always in iife
+
+//NOTE-> for reduce the global polution and immidate execution, we use iife function 
+// ()execution ()declaration 
+
+( (name) => {
+    //simple or unnammed iife
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('jaydeep')
