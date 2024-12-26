6f944f53f15d4238d0c52b48869a68fbbdad434c
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2024-12-26 15:42:37 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,10 +7,56 @@ if (true) {
     
 }
 
-
-
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
 //NOTE--> NODE scope (code enviroment) OR BROWSER SCOPE ARE DIFFRENT.
+
+
+//Nested scope
+function one(){
+    const username = "happy"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "happy"
+    if (username === "happy") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){ //only function
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){ //variable hold
+    return num + 2
+}
+
+
+//hoesting ? --> explore more
