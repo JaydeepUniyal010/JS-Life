6873ac528b590fc38bb433f3018432c75e9cfb82
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2025-01-16 23:51:08 +0530

    Update 06_nums_and_math.js

--- 01_StartBasic/06_nums_and_math.js
+++ 01_StartBasic/06_nums_and_math.js
@@ -1,34 +1,37 @@
-const score = 400
-// console.log(score);
+const score = 400 
+// console.log(score); //400
 
-const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(100)   //explict declare by us
+// console.log(balance);  //number:400
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toString().length); //convert in string
+// console.log(balance.toFixed(1)); //100.4 =>100(fix)
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4)); //123.8966 ==> 124, 
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN')); //100000==> 10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
+// console.log(Math.abs(-4)); //-4 ==> 4
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+
+// math.random (Tmportant)
+console.log(Math.random());// value betwwn 0 and 1.
+console.log((Math.random()*10) + 1); //+1 for avoid 0
+console.log(Math.floor(Math.random()*10) + 1); //exact value
 
 const min = 10
 const max = 20
 
+//IMPORTANT FORMULA
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
