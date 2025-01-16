6a12958e178567697df81589117f71980fc855d5
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2025-01-16 22:44:37 +0530

    Update 04_comparision.js

--- 01_StartBasic/04_comparision.js
+++ 01_StartBasic/04_comparision.js
@@ -4,13 +4,15 @@
 // console.log(2<=1);
 // console.log(2!=1);
 
-// console.log ("2" >1);
+// console.log ("2" >1);  //not allow to compare two different type data types
 // console.log("02" >1);
+
+// note => not allow to compare two different type data type.
 
 // // always avoid this type code and rembember clean code
 // console.log(null > 0);
 // console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null >= 0); //true but why?
 
 // //always avoid this type code
 // console.log(undefined ==0);
