550594fc2032c91b4ac0ba9b13676ed9dbf3aff3
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2025-01-17 00:09:07 +0530

    Update 07_datesinJs.js

--- 01_StartBasic/07_datesinJs.js
+++ 01_StartBasic/07_datesinJs.js
@@ -1,6 +1,6 @@
 // Dates
 
-let myDate = new Date()
+let myDate = new Date()  //declare Date object instances
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
@@ -9,16 +9,17 @@ let myDate = new Date()
 //let myCreated = new Date(2023,0,20)
 //let myCreated = new Date(2023,0,20,4,3)
 //let myCreated = new Date("2024-01-15")
-let myCreatedDate = new Date("01-14-2024")
+let myCreatedDate = new Date("01-14-2024") //more specfic
 //console.log(myCreated.toLocaleString());
 
 //TimeStamps
-let myTimestamp= Date.now();
+let myTimestamp= Date.now(); 
 //console.log(myTimestamp);
 //console.log(myCreatedDate.getTime());
-//console.log(Math.floor(Date.now()/1000));
+//console.log(Math.floor(Date.now()/1000)); //important topic for interview (ms to sec)
 
-//
+//some important other
+
 let newDate = new Date()
 //console.log(newDate);
 //console.log(newDate.getDay());
@@ -26,8 +27,10 @@ let newDate = new Date()
 //console.log(newDate.getMonth() +1);
 
 //String interpolutation
-//`${newDate.getDay()} "and the time is"`
+//`${newDate.getDay()} "and the time is"` //print date
 
+
+//date method =========> IMPORTANT //interalization
 newDate.toLocaleString('default', {
     weekday:"long",    
 })
