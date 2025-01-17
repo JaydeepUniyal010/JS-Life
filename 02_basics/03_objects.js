d0b3d4adf85e5a932623e0c65e38231a7815c5fd
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2025-01-17 18:27:37 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,36 +3,36 @@
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1") //declare symbol
 
 
-const JsUser = {
+const JsUser = { //
     name: "jaydeep",
-    "full name": "happy Jaydeep",
-    [mySym]: "mykey1",
+    "full name": "happy Jaydeep", //
+    [mySym]: "mykey1", //symbol use a key and put square brackets
     age: 18,
-    location: "Jaipur",
+    location: "Dehradun",
     email: "happy@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
+// console.log(JsUser.email) //object access 1st but not good way
 // console.log(JsUser["email"])
-// console.log(JsUser["full name"])
+// console.log(JsUser["full name"]) // never access with . or (jsuser.fullname)
 // console.log(JsUser[mySym])
 
-JsUser.email = "happy@chatgpt.com"
-// Object.freeze(JsUser)
+JsUser.email = "happy@chatgpt.com" //change the object value
+// Object.freeze(JsUser)              // freeze ===> not changes after this
 JsUser.email = "happy@microsoft.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
+JsUser.greeting = function(){ // 
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); //this ===> for same object refrence
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting()); //
+console.log(JsUser.greetingTwo()); //
