09d744f787691eaac90eddea2c94b0ccd5d27c46
JaydeepUniyal010 <141512883+JaydeepUniyal010@users.noreply.github.com>
2025-01-17 17:36:11 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,7 +6,7 @@ const myHeors = ["shaktiman", "naagraj"]
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
-// Array methods
+// Array methods ===> 
 
 // myArr.push(6)
 // console.log(myArr);
@@ -16,24 +16,24 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr);
 
 
-// myArr.pop()
-// console.log(myArr); 
+// myArr.pop()  //not give the value
+// console.log(myArr);  
 
 
-// myArr.unshift(9)
+// myArr.unshift(9)  // why unshift not so good
 // console.log(myArr);
 
 
 // myArr.shift()
 // console.log(myArr);
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); // 9 include hai ya nahi?
+// console.log(myArr.indexOf(3)); // which index?
 
 // const newArr = myArr.join() //important*  string convert
 
 // console.log(myArr);
-// console.log( newArr);
+// console.log( newArr); //join change array to string
 
 
 // slice, splice
@@ -46,6 +46,6 @@ console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3)  //manipulate the real array means value gayab
 console.log("C ", myArr);
 console.log(myn2);
